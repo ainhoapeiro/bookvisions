2f0e1f8e6c4f6115291ea6c1881aa1fe9a66400c
Ainhoa Peiro <23.daw.ainhoapeiro@fundacionmontessori.com>
2025-05-27 00:17:03 +0200

    base de datos

--- database/migrations/2025_05_05_164501_create_follows_table.php
+++ database/migrations/2025_05_05_164501_create_follows_table.php
@@ -6,25 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('follows', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('follower_id')->constrained()->constrained('users')->onDelete('cascade');
-            $table->foreignId('followed_id')->constrained()->constrained('users')->onDelete('cascade');
+            $table->foreignId('follower_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('followed_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
 
             $table->unique(['follower_id', 'followed_id']);
         });
-
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('follows');
