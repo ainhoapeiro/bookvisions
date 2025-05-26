219d4f1651850ecb0dd6f9fb628a749008121fc7
Ainhoa Peiro <23.daw.ainhoapeiro@fundacionmontessori.com>
2025-05-26 21:09:34 +0200

    Tabla comentarios

--- database/migrations/2025_05_05_164460_create_comments_table.php
+++ database/migrations/2025_05_05_164460_create_comments_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->foreignId('illustration_id')->constrained()->onDelete('cascade');
+            $table->foreignId('illustration_id')->constrained('illustrations')->onDelete('cascade');
             $table->text('content');
             $table->timestamps();
         });
