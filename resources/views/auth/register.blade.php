f995430ac90e50998f3df3f94dbd4586bacdeac3
Ainhoa Peiro <23.daw.ainhoapeiro@fundacionmontessori.com>
2025-05-27 23:16:08 +0200

    cambios registro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,6 @@
 <x-app-layout>
-    <div class="text-xl text-center mt-20 text-purple-700">Estoy en el registro 🎨</div>
-
-    <div class="mt-10">
+    <div class="max-w-3xl mx-auto py-12">
         <livewire:register />
     </div>
 </x-app-layout>
+
